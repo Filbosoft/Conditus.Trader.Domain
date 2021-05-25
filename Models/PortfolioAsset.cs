fbb95d5a4cf38b4e91f0716d69a105b2b5afcb3e
PBonvang <peter.bonvang@gmail.com>
2021-05-25 19:04:01 +0200

    PortfolioAsset id/hash property added;

--- Models/PortfolioAsset.cs
+++ Models/PortfolioAsset.cs
@@ -1,7 +1,10 @@
+using Amazon.DynamoDBv2.DataModel;
+
 namespace Conditus.Trader.Domain.Models
 {
     public class PortfolioAsset
     {
+        [DynamoDBHashKey]
         public string Symbol { get; set; }
         public string Name { get; set; }
         public int Quantity { get; set; }
