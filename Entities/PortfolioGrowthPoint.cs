0be8d36965bf82d39773eb07df999e82853aab84
PBonvang <peter.bonvang@gmail.com>
2021-05-27 23:51:18 +0200

    PortfolioGrowthPoint timestamp updated to GrowthPointTimestamp;

--- Entities/PortfolioGrowthPoint.cs
+++ Entities/PortfolioGrowthPoint.cs
@@ -14,6 +14,6 @@ namespace Conditus.Trader.Domain.Entities
         [DynamoDBProperty]
         public string OwnerId { get; set; }
         [DynamoDBRangeKey(typeof(DateTimePropertyConverter))]
-        public DateTime Timestamp { get; set; }
+        public DateTime GrowthPointTimestamp { get; set; }
     }
 }
